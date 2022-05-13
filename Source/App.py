c7a0a2bda3ecc71033ec6951fc92bfdbd6ce8ce9
fk102 <fayyadhk103@gmail.com>
2022-05-13 08:12:38 +0600

    Working build

--- Source/App.py
+++ Source/App.py
@@ -3,10 +3,10 @@ import os
 
 sg.theme("darkgray 12")
 layout = [[sg.Text("Time to optimize your programs!")],
-          [sg.Input("Select shorcut save location", key='-IN-'),
+          [sg.Input("Select shorcut save location", key='-IN-', do_not_clear=False),
            sg.FolderBrowse("Browse")],
-          [sg.Input("Select location of the program", key="-out-"),
-           sg.FileBrowse("Browse")], [sg.Input("Enter Shortcut name", key="name")],
+          [sg.Input("Select location of the program", key="-out-", do_not_clear=False),
+           sg.FileBrowse("Browse")], [sg.Input("Enter Shortcut name", key="name", do_not_clear=False)],
           [sg.Button("Optimize")]]
 
 window = sg.Window("Program", layout)
@@ -25,4 +25,4 @@ while True:
         f = open(newtarget, 'x')
         f.write(core_set+path)
         f.close()
-window.close()
+        window.close()
